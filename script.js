556a019298510817ba5a5e728481e6bec7223bec
Tal Poris <119438047+TalPO24@users.noreply.github.com>
2023-03-12 13:49:58 +0200

    JavaScript Fundamentals

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 /*
+//* VALUES and VARIABELS
+
 let js = "amazing";
 // if (js === "amazing") alert("java script is fun");
 console.log(40 + 8 + 23 - 10)
@@ -10,14 +12,7 @@ console.log(23)
 let firstName = "jonas"
 console.log(firstName)
 
- //* TK-1
-let country = "Israel";
-let continent = "Middle East"
-let population = 9000000
 
-console.log(country)
-console.log(continent)
-console.log(population)
 
 let javaScriptIsFun = true;
 console.log(javaScriptIsFun)
@@ -26,6 +21,10 @@ console.log(javaScriptIsFun)
 console.log(typeof javaScriptIsFun)
 // console.log(typeof 23)
 // console.log(typeof 'Jonas')
+
+
+
+//* DATA TYPES
 
 //* if we want to change the veriable we do it without "let" because let is already defined.
 javaScriptIsFun = "YES!"
@@ -53,3 +52,45 @@ job = 'teacher'
 lastName = 'poris'
 console.log(lastName)
 */
+
+
+//* MATH OPERATORS
+const now = 2037
+const ageTal = now - 1996
+const ageSarah = now - 2018
+console.log(ageTal, ageSarah)
+
+console.log(ageTal * 2, ageTal / 10, 2 ** 3)
+    //* 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = "Tal"
+const lastName = "Poris"
+console.log(firstName + " " + lastName)
+
+//* ASSIGNMENT OPERATORS
+let x = 10 + 5 //15
+x += 10; // x = x + 10 = 25
+x *= 4 // x = x * 4 = 100
+x++; // x = x + 1
+x--; // x = x - 1
+x--; // x = x - 1
+console.log(x)
+
+
+//* COMPARISON OPERATORS
+console.log(ageTal > ageSarah) // >, <, >=, <=
+console.log(ageSarah >= 18)
+
+const isFullAge = ageSarah >= 18
+console.log(now - 196 > now - 2018)
+
+
+
+//* TK-1
+//  let country = "Israel";
+//  let continent = "Middle East"
+//  let population = 9000000
+
+//  console.log(country)
+//  console.log(continent)
+//  console.log(population)
