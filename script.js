73367d9c82b119c6da430fe580db291c4c098dea
Tal Poris <119438047+TalPO24@users.noreply.github.com>
2023-03-12 13:21:32 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,55 @@
+/*
+let js = "amazing";
+// if (js === "amazing") alert("java script is fun");
+console.log(40 + 8 + 23 - 10)
+
+console.log("jonas")
+console.log(23)
+
+//* we assigned  a veriable (firstName) and declared that is equal to "jonas" (firstName = "jonas").
+let firstName = "jonas"
+console.log(firstName)
+
+ //* TK-1
+let country = "Israel";
+let continent = "Middle East"
+let population = 9000000
+
+console.log(country)
+console.log(continent)
+console.log(population)
+
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun)
+
+// console.log(typeof true)
+console.log(typeof javaScriptIsFun)
+// console.log(typeof 23)
+// console.log(typeof 'Jonas')
+
+//* if we want to change the veriable we do it without "let" because let is already defined.
+javaScriptIsFun = "YES!"
+console.log(typeof javaScriptIsFun)
+
+let year;
+console.log(year)
+console.log(typeof year)
+year = 1991
+console.log(typeof year)
+
+console.log(typeof null)
+
+let age = 30
+age = 31
+
+const birthYear = 1996
+// birthYear = 1997
+// const job;
+
+
+var job = 'programmer'
+job = 'teacher'
+
+lastName = 'poris'
+console.log(lastName)
+*/
