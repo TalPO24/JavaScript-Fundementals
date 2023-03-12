a0e63047752a84ac0049bbefeda35ecf113e1c5d
Tal Poris <119438047+TalPO24@users.noreply.github.com>
2023-03-12 19:18:36 +0200

    JS Fundementals

--- script.js
+++ script.js
@@ -196,6 +196,7 @@ if (markBmi > johnBmi) {
 */
 
 //* TYPE CONVERSION 
+/*
 const inputYear = '1991'
 console.log(Number(inputYear), inputYear)
 console.log(Number(inputYear) + 18)
@@ -209,3 +210,28 @@ console.log(String(23), 23)
 console.log('I am ' + 23 + ' years old')
 console.log('23' - '10' - 3)
 console.log('23' * '2')
+*/
+
+
+//* TRUTHY and FALSY VALUES
+// 5 falsy values: 0,'', undefined, null, NaN
+
+console.log(Boolean(0))
+console.log(Boolean(undefined))
+console.log(Boolean('Jonas'))
+console.log(Boolean({}))
+console.log(Boolean(''))
+
+const money = 100
+if (money) {
+    console.log("Don't spend it all")
+} else {
+    console.log("you should get a job")
+}
+
+let height = 0
+if (height) {
+    console.log('YAY! Height is defined')
+} else {
+    console.log('Height is undefined')
+}
