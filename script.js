758760ac46f98e4d10c806ca2b6618b2a66d8abf
Tal Poris <119438047+TalPO24@users.noreply.github.com>
2023-03-13 14:38:11 +0200

    JS Fundementals

--- script.js
+++ script.js
@@ -215,7 +215,7 @@ console.log('23' * '2')
 
 //* TRUTHY and FALSY VALUES
 // 5 falsy values: 0,'', undefined, null, NaN
-
+/*
 console.log(Boolean(0))
 console.log(Boolean(undefined))
 console.log(Boolean('Jonas'))
@@ -234,4 +234,31 @@ if (height) {
     console.log('YAY! Height is defined')
 } else {
     console.log('Height is undefined')
+}
+*/
+
+
+//* EQUALITY OPERATORS == VS ===
+
+const age = 18
+if (age === 18)
+    console.log("You just bacame an adult (strict)")
+if (age == 18)
+    console.log("You just bacame an adult(loose)")
+
+const favourite = Number(prompt("what's your favourite number?"))
+console.log(typeof favourite)
+
+if (favourite === 23) {
+    console.log('Cool! 23 is an amazing number')
+} else if (favourite === 7) {
+    console.log(" 7 is also a cool number")
+} else if (favourite === 9) {
+    console.log(" 9 is also a cool number")
+} else {
+    console.log("number is not 23 or 7 or 9")
+}
+
+if (favourite !== 23) {
+    console.log("Why not 23?")
 }
